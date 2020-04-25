79964a23f1bdab5711f04a3a41227795c74ae0e9
kristianpedersen <kristianp89@gmail.com>
2020-04-25 20:31:05 +0200

    It works!

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
 // https://makecode.com/_Cm17aw5xbKpU
+// https://makecode.com/_1oqHvKFEE9Lf
 
-setTimeout(main, 2)
+const canvas = document.querySelector("canvas")
+const ta = document.querySelector("textarea")
+const copyButton = document.querySelector("button")
 
-function main() {
+document.querySelector("input").addEventListener("change", function () {
+	copyButton.innerText = "Copy code"
+	const img = document.createElement("img")
+	img.src = window.URL.createObjectURL(this.files[0])
+	img.addEventListener("load", () => main(img))
+})
+
+function main(img) {
 	const arcadeColors = [
 		"#000000",
 		"#ffffff",
-		"#EB0B32",// "#ff2121", 
+		"#ff2121",
 		"#ff93c4",
 		"#ff8135",
 		"#fff609",
@@ -21,27 +31,19 @@ function main() {
 		"#91463d",
 		"#000000",
 	].map((color, index) => {
+		const r = parseInt(color[1] + color[2], 16)
+		const g = parseInt(color[3] + color[4], 16)
+		const b = parseInt(color[5] + color[6], 16)
 		return {
-			color: hexToHSL(color),
+			color: { r, g, b },
 			index: (index).toString(16) // 9 = 9, 10 = a, 11 = b, etc.
 		}
 	})
 
-	console.table(arcadeColors[2])
+	const factor = img.width / 120
+	img.width = 120
+	img.height /= factor
 
-	const form = document.querySelector("input")
-
-	const MAX_IMAGE_SIZE = 180
-	const img = document.querySelector("img")
-
-	const factor =
-		Math.min(img.width, img.height) / Math.max(img.width, img.height)
-
-	img.width -= img.width - MAX_IMAGE_SIZE
-	img.height -= img.height - MAX_IMAGE_SIZE
-	//  img.width *= factor
-
-	const canvas = document.querySelector("canvas")
 	canvas.width = img.width
 	canvas.height = img.height
 	const c = canvas.getContext("2d")
@@ -49,41 +51,38 @@ function main() {
 	c.drawImage(img, 0, 0, canvas.width, canvas.height)
 	const imgData = c.getImageData(0, 0, canvas.width, canvas.height)
 	const data = imgData.data
+	c.fillStyle = "black"
+	c.fillRect(0, 0, canvas.width, canvas.height)
 
 	let pixelIndex = 0
-	let x = 0
-	let y = 0
 	const makeCodeString = {}
 
 	for (let i = 0; i < data.length; i += 4) {
-		x = pixelIndex % canvas.width
-		y = Math.floor(pixelIndex / canvas.height)
+		let x = pixelIndex % canvas.width
+		let y = Math.floor(pixelIndex / canvas.width)
 
 		const r = data[i + 0]
 		const g = data[i + 1]
 		const b = data[i + 2]
 		const a = data[i + 3]
 
-		const hexString = "#" + [r, g, b]
-			.map(n => n.toString(16))
-			.join("")
-		const pixelAsHSL = hexToHSL(hexString)
-
-		const nearestL = arcadeColors.sort((a, b) => {
-			const hDifference = Math.abs(a.color.h - pixelAsHSL.h) - Math.abs(b.color.h - pixelAsHSL.h)
-			const sDifference = Math.abs(a.color.s - pixelAsHSL.s) - Math.abs(b.color.s - pixelAsHSL.s)
-			const lDifference = Math.abs(a.color.l - pixelAsHSL.l) - Math.abs(b.color.l - pixelAsHSL.l)
-			return (hDifference * 0.2) + (sDifference * 0.2) + (lDifference * 200)
+		const nearest = Array.from(arcadeColors).sort((prev, curr) => {
+			const rDifference = Math.abs(prev.color.r - r) - Math.abs(curr.color.r - r)
+			const gDifference = Math.abs(prev.color.g - g) - Math.abs(curr.color.g - g)
+			const bDifference = Math.abs(prev.color.b - b) - Math.abs(curr.color.b - b)
+			return rDifference + gDifference + bDifference
 		})[0]
 
-		const nearestAsHSLString = `hsl(${nearestL.color.h}, ${Math.floor(nearestL.color.s)}%, ${Math.floor(nearestL.color.l)}%)`
-		c.fillStyle = nearestAsHSLString
+		c.fillStyle = `rgb(${nearest.color.r}, ${nearest.color.g}, ${nearest.color.b})`
 		c.fillRect(x, y, 1, 1)
 
 		if (makeCodeString[`row-${y}`] === undefined) {
 			makeCodeString[`row-${y}`] = ""
 		} else {
-			makeCodeString[`row-${y}`] += nearestL.index + "\t"
+			// if (pixelIndex == 100000) {
+			// 	console.log(makeCodeString[`row-${y}`])
+			// }
+			makeCodeString[`row-${y}`] += nearest.index + "\t"
 		}
 
 		pixelIndex++
@@ -95,51 +94,12 @@ function main() {
 	}
 	finalOutput += "`, SpriteKind.Player)"
 
-	console.log(finalOutput)
+	ta.textContent = finalOutput
+	copyButton.removeAttribute("disabled")
 
-	// hexToHSL: https://css-tricks.com/converting-color-spaces-in-javascript/
-	function hexToHSL(H) {
-		// Convert hex to RGB first
-		let r = 0, g = 0, b = 0;
-		if (H.length == 4) {
-			r = "0x" + H[1] + H[1];
-			g = "0x" + H[2] + H[2];
-			b = "0x" + H[3] + H[3];
-		} else if (H.length == 7) {
-			r = "0x" + H[1] + H[2];
-			g = "0x" + H[3] + H[4];
-			b = "0x" + H[5] + H[6];
-		}
-		// Then to HSL
-		r /= 255;
-		g /= 255;
-		b /= 255;
-		let cmin = Math.min(r, g, b),
-			cmax = Math.max(r, g, b),
-			delta = cmax - cmin,
-			h = 0,
-			s = 0,
-			l = 0;
-
-		if (delta == 0)
-			h = 0;
-		else if (cmax == r)
-			h = ((g - b) / delta) % 6;
-		else if (cmax == g)
-			h = (b - r) / delta + 2;
-		else
-			h = (r - g) / delta + 4;
-
-		h = Math.round(h * 60);
-
-		if (h < 0)
-			h += 360;
-
-		l = (cmax + cmin) / 2;
-		s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
-		s = +(s * 100).toFixed(1);
-		l = +(l * 100).toFixed(1);
-
-		return { h, s, l };
-	}
+	copyButton.addEventListener("click", () => {
+		ta.select()
+		document.execCommand("copy")
+		copyButton.innerText = "Code copied to clipboard!"
+	})
 }
